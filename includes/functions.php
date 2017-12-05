22a67880d0a5368902527e886f601da3ec0c843f
System Administrator <root@Ms-MacBook-Pro.local>
2017-12-05 03:32:31 -0700

    Changes in code

--- includes/functions.php
+++ includes/functions.php
@@ -5,6 +5,20 @@
 		exit;
 	}
 
+	function connect_db()
+	{
+		define('DB_SERVER', 'trendzy.cgtircrxumcl.us-east-1.rds.amazonaws.com');
+		define('DB_USERNAME', 'trendzyuser');
+		define('DB_PASSWORD', 'trendzyuser');
+		define('DB_DATABASE', 'Trendzy');
+
+		// $connection = mysql_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD) or die(mysql_error());
+		// $database = mysql_select_db(DB_DATABASE) or die(mysql_error());
+
+		$connection = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE) or die(mysql_error());
+
+		return $connection;
+	}
 
 	function search_db($table, $attribute, $item)
 	{
@@ -15,7 +29,6 @@
 			$tmp = "set @tmp = SHA1('$item');";
 			$query = "select id from {$table} where password=@tmp;";
 			$result = mysqli_query($connection, $tmp);
-
 		}
 		else
 		{
--- public/register.php
+++ public/register.php
@@ -4,26 +4,18 @@
 		<title>Register</title>
 	</head>
 	<body>
-		<?php
-			if(isset($_GET['host']))
-			{
-				$db_host = $_GET['host'];
-				$db_user = $_GET['user'];
-				$db_pass = $_GET['pass'];
-				$db_name = $_GET['name'];
-			}
-			else
-			{
-				$db_host = $_POST['host'];
-				$db_user = $_POST['user'];
-				$db_pass = $_POST['pass'];
-				$db_name = $_POST['name'];
-			}
+		<?php 
+			if($_GET["again"] == 1) 
+			{ 
+				echo "<h3>Please try again</h3>";
+				echo "<ul>";
+				if($_GET["user"] == 1) 
+					echo "<li><p>Username exists</p></li>";
+				if($_GET["email"] == 1) 
+					echo "<li><p>Email exists</p></li>";
+				echo "</ul>";
+			} 
 		?>
-
-		<?php if($_GET["again"] == 1) { ?>
-			<h3>Please try again</h3>
-		<?php } ?>
 
 		<form action="../includes/register_process.php" method="post">
 			Username: <input type="text" name="username" value=""> <br>
@@ -31,12 +23,11 @@
 			First Name: <input type="text" name="fname" value=""> <br>
 			Last Name: <input type="text" name="lname" value=""> <br>
 			Password: <input type="password" name="password" value=""> <br>
-			<br>
-			<input type="hidden" name="host" value="<?php echo $db_host ?>">
-			<input type="hidden" name="user" value="<?php echo $db_user ?>">
-			<input type="hidden" name="pass" value="<?php echo $db_pass ?>">
-			<input type="hidden" name="name" value="<?php echo $db_name ?>">
 			<input type="submit" name="submit" value="Submit">
 		</form>
+
+		<br>
+		
+		<a href="log_in.php">Log in</a>
 	</body>
 </html>
