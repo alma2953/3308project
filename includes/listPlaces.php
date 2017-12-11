5f5989520972e850e2580c15f15eab28bf8dc35c
System Administrator <root@econ1-181-28-dhcp.int.colorado.edu>
2017-12-11 12:59:26 -0700

    Fixed listPlaces.php

--- includes/listPlaces.php
+++ includes/listPlaces.php
@@ -7,13 +7,13 @@
 		$query = "select * from Places where user_id='{$user_id}';";
 
 		$result = mysqli_query($connection, $query);
-		$placesArray = [];
+		$placesArray = array();
 		while($row = mysqli_fetch_assoc($result))
 		{
 			$array = array('name' => $row['location'],'lat' => $row['latitude'],'lng' => $row['latitude']);
 			array_push($placesArray, $array);
 		}
-		echo json_encode($array);
+		echo json_encode($placesArray);
 
 ?>
 
