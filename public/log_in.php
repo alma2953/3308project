b4835964ef9a55301bee9aafc2502a1aaf8a6c2f
System Administrator <root@engr2-6-101-dhcp.int.colorado.edu>
2017-12-11 14:19:20 -0700

    frontend for register and loing

--- public/log_in.php
+++ public/log_in.php
@@ -2,6 +2,7 @@
 <html>
 	<head>
 		<title>Log in</title>
+		<link rel="stylesheet" type="text/css" href="stylesheets/style1.css">
 	</head>
 	<body>
 		<?php if($_GET["again"] == 1) { ?>
@@ -11,11 +12,11 @@
 		<form action="../includes/log_in_process.php" method="post">
 			Username: <input type="text" name="username" value=""> <br>
 			Password: <input type="password" name="password" value=""> <br>
-			<input type="submit" name="submit" value="Submit">
+			<input type="submit" name="submit" value="Submit"> <br><br>
+			<a href="register.php" style="font-size: 15px;">Register</a>
 		</form>
 
 		<br>
 
-		<a href="register.php">Register</a>
 	</body>
 </html>
--- public/register.php
+++ public/register.php
@@ -2,6 +2,7 @@
 <html>
 	<head>
 		<title>Register</title>
+		<link rel="stylesheet" type="text/css" href="stylesheets/style1.css">
 	</head>
 	<body>
 		<?php 
@@ -20,16 +21,15 @@
 		?>
 
 		<form action="../includes/register_process.php" method="post">
-			Username: <input type="text" name="username" value=""> <br>
-			Email : <input type="text" name="email" value=""> <br>
-			First Name: <input type="text" name="fname" value=""> <br>
-			Last Name: <input type="text" name="lname" value=""> <br>
-			Password: <input type="password" name="password" value=""> <br>
+			Username<br> <input type="text" name="username" value=""> <br>
+			Email<br> <input type="text" name="email" value=""> <br>
+			First Name<br> <input type="text" name="fname" value=""> <br>
+			Last Name<br> <input type="text" name="lname" value=""> <br>
+			Password<br> <input type="password" name="password" value=""> <br>
 			<input type="submit" name="submit" value="Submit">
 		</form>
 
 		<br>
 		
-		<a href="log_in.php">Log in</a>
 	</body>
 </html>
